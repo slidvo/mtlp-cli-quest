f694ad33aa5195bdf199f4eea1bca862e62a684d
Vyacheslav Kopeyka <38747054+slidvo@users.noreply.github.com>
2026-02-23 14:30:25 +0500

    refactor(core): :recycle: remove unused field from State

--- src/State.ts
+++ src/State.ts
@@ -1,12 +1,9 @@
-import { SceneTitle } from "./enums/indext.js";
 export default interface State {
-  sceneTitle: SceneTitle;
   money: number;
   health: number;
 }
 
 export const state: State = {
-  sceneTitle: SceneTitle.START,
   money: 0,
   health: 100,
 };
